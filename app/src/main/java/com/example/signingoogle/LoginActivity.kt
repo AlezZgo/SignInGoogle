f11269c790d8697081a01e31e0cbfbe058baa054
Alexander <alez.grante@gmail.com>
2022-03-02 00:19:51 +0300

    token id

--- app/src/main/java/com/example/signingoogle/LoginActivity.kt
+++ app/src/main/java/com/example/signingoogle/LoginActivity.kt
@@ -1,11 +1,64 @@
 package com.example.signingoogle
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.signingoogle.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        val options = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN
+        ).requestServerAuthCode(resources.getString(R.string.server_client_id))
+            .requestEmail()
+            .requestIdToken(resources.getString(R.string.server_client_id))
+            .build()
+
+        val client = GoogleSignIn.getClient(this,options)
+
+        binding.signInButton.setOnClickListener {
+            val intent = client.signInIntent
+            startActivityForResult(intent,100)
+        }
+        setContentView(binding.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==100){
+
+            val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account = task.getResult(ApiException::class.java)
+            okResult(account)
+        }catch (e: Exception){
+            okResult(null)
+        }
+    }
+
+    fun okResult(account: GoogleSignInAccount?){
+        if(account==null){
+            binding.textView.text = "noname"
+        }else{
+            binding.textView.text = account.id.toString()
+        }
     }
 }
